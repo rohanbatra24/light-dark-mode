c1338b42c0bd0398159f36bbd7ede68bffa6b952
Rohan Batra <rohanbatra24@gmail.com>
2021-05-16 00:48:15 -0400

    save theme to local storage

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
-// Update html attributes
+// Save theme to local storage
 
+const saveToLocalStorage = (color) => {
+  localStorage.setItem("theme", color);
+};
+
+// Update html attributes
 const updateAttributes = (element, attr, value) => {
   element[attr] = value;
 };
 
 // Dark or light images
-
 const imageMode = (color) => {
   image1.src = `img/undraw_proud_coder_${color}.svg`;
   image2.src = `img/undraw_feeling_proud_${color}.svg`;
@@ -21,7 +25,6 @@ const imageMode = (color) => {
 };
 
 // Dark mode styles
-
 const darkMode = () => {
   updateAttributes("nav", "style.backgroundColor", "rgb(0 0 0 / 50%)");
   //   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
@@ -34,7 +37,6 @@ const darkMode = () => {
 };
 
 // Light mode styles
-
 const lightMode = () => {
   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
@@ -47,16 +49,27 @@ const lightMode = () => {
 
 // switch Theme Dynamically
 const switchTheme = (event) => {
-  console.log(`event`, event.target.checked);
-
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    saveToLocalStorage("dark");
+
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    saveToLocalStorage("light");
     lightMode();
   }
 };
 
 // Event listener
 toggleSwitch.addEventListener("change", switchTheme);
+
+// CHeck local storage for theme
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  document.documentElement.setAttribute("data-theme", savedTheme);
+  if (savedTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
