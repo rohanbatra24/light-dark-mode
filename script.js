1c15075e2ea507c18902f9900314c1c8a1927975
Rohan Batra <rohanbatra24@gmail.com>
2021-05-16 00:30:26 -0400

    create function to switch images from light/dark

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ const updateAttributes = (element, attr, value) => {
   element[attr] = value;
 };
 
+// Dark or light images
+
+const imageMode = (color) => {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+};
+
 // Dark mode styles
 
 const darkMode = () => {
@@ -22,9 +30,7 @@ const darkMode = () => {
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
 
-  image1.src = "img/undraw_proud_coder_dark.svg";
-  image2.src = "img/undraw_feeling_proud_dark.svg";
-  image3.src = "img/undraw_conceptual_idea_dark.svg";
+  imageMode("dark");
 };
 
 // Light mode styles
@@ -36,9 +42,7 @@ const lightMode = () => {
   toggleIcon.children[0].textContent = "Light Mode";
   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
 
-  image1.src = "img/undraw_proud_coder_light.svg";
-  image2.src = "img/undraw_feeling_proud_light.svg";
-  image3.src = "img/undraw_conceptual_idea_light.svg";
+  imageMode("light");
 };
 
 // switch Theme Dynamically
